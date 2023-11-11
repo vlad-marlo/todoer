f93974c75acd606304a718fd3956e930703cbb5b
Vlad Marlo <vlad.marlo@ya.ru>
2023-11-11 22:50:57 +0300

    added create task service handler

--- internal/service/tasks.go
+++ internal/service/tasks.go
@@ -2,12 +2,39 @@ package service
 
 import (
 	"context"
+	"fmt"
+	"github.com/google/uuid"
 	"github.com/vlad-marlo/todoer/internal/model"
+	"net/http"
+	"time"
 )
 
-func (s *Service) Create(ctx context.Context, task string, status model.Status) (*model.CreateTaskResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *Service) Create(ctx context.Context, taskValue string, status model.Status) (*model.CreateTaskResponse, error) {
+	var (
+		now  = time.Now().UTC()
+		id   = uuid.New()
+		task = &model.TaskDTO{
+			ID:        id,
+			Value:     taskValue,
+			CreatedAt: now,
+			Status:    status,
+		}
+	)
+
+	err := s.storage.Task().Create(ctx, task)
+	if err != nil {
+		return nil, model.ErrorMessage{
+			Endpoint: "/api/v1/tasks POST",
+			Code:     http.StatusInternalServerError,
+			Status:   fmt.Sprintf("unknown error in storage layer: %v", err),
+		}
+	}
+
+	return &model.CreateTaskResponse{
+		ID:        id,
+		Task:      taskValue,
+		CreatedAt: now,
+	}, nil
 }
 
 func (s *Service) GetMany(ctx context.Context, offset, limit int, task string, statuses ...model.Status) (*model.GetTasksResponse, error) {
@@ -30,7 +57,7 @@ func (s *Service) Delete(ctx context.Context, id string) error {
 	panic("implement me")
 }
 
-func (s *Service) ChangeStatus(ctx context.Context, id, status string) (*model.TaskDTO, error) {
+func (s *Service) ChangeStatus(ctx context.Context, id string, status model.Status) (*model.TaskDTO, error) {
 	//TODO implement me
 	panic("implement me")
 }
--- internal/storage/interface.go
+++ internal/storage/interface.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"context"
+	"github.com/vlad-marlo/todoer/internal/model"
+)
+
 // Storage is interface of storage, which can be used in application.
 type Storage interface {
 	Task() TaskRepository
@@ -8,4 +13,5 @@ type Storage interface {
 // TaskRepository is interface of object, which
 // is storing and returning tasks.
 type TaskRepository interface {
+	Create(ctx context.Context, task *model.TaskDTO) error
 }
