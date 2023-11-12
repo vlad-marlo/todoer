4b072f4310af76878641871de4e2d3524385d436
Vlad Marlo <vlad.marlo@ya.ru>
2023-11-12 19:45:31 +0300

    added filtrations and sorting

--- internal/storage/pgx/taskrepository.go
+++ internal/storage/pgx/taskrepository.go
@@ -2,6 +2,7 @@ package pgx
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/vlad-marlo/todoer/internal/model"
@@ -25,7 +26,7 @@ func newTask(log *zap.Logger, pool *pgxpool.Pool) *TaskRepository {
 func (t *TaskRepository) Get(ctx context.Context, id uuid.UUID) (*model.TaskDTO, error) {
 	const query = `SELECT id, value, created_at, status
 FROM tasks
-where id = $1;`
+WHERE id = $1;`
 
 	var (
 		task   = new(model.TaskDTO)
@@ -73,9 +74,48 @@ WHERE id = $3;`
 	return nil
 }
 
+func (t *TaskRepository) getManyTasksByQuery(ctx context.Context, query string, args ...any) ([]model.TaskDTO, error) {
+	var res []model.TaskDTO
+
+	rows, err := t.pool.Query(ctx, query, args...)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var (
+			task   model.TaskDTO
+			status int
+		)
+		if err = rows.Scan(&task.ID, &task.Value, &task.CreatedAt, &status); err != nil {
+			return nil, err
+		}
+		task.Status = model.StatusFromInt(status)
+		res = append(res, task)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 func (t *TaskRepository) Paginate(ctx context.Context, offset int, limit int) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+ORDER BY created_at DESC
+OFFSET $1 LIMIT $2;`
+	return t.getManyTasksByQuery(ctx, query, offset, limit)
+}
+
+func (t *TaskRepository) PaginateFilter(ctx context.Context, offset int, limit int, task string) ([]model.TaskDTO, error) {
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE value LIKE $1
+ORDER BY created_at DESC
+OFFSET $2 LIMIT $3;`
+	return t.getManyTasksByQuery(ctx, query, fmt.Sprintf("%%%s%", task), offset, limit)
 }
 
 func (t *TaskRepository) CreateMany(ctx context.Context, tasks []model.TaskDTO) error {
@@ -88,37 +128,56 @@ func (t *TaskRepository) ChangeStatus(ctx context.Context, id uuid.UUID, status 
 	panic("implement me")
 }
 
-func (t *TaskRepository) PaginateFilter(ctx context.Context, offset int, limit int, task string) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
 func (t *TaskRepository) PaginateWithStatus(ctx context.Context, offset int, limit int, status model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE status = $1
+ORDER BY created_at DESC
+OFFSET $2 LIMIT $3;`
+	return t.getManyTasksByQuery(ctx, query, status.Int(), offset, limit)
 }
 
 func (t *TaskRepository) PaginateWithStatuses(ctx context.Context, offset int, limit int, status1, status2 model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE status = $1 OR status = $2
+ORDER BY created_at DESC
+OFFSET $3 LIMIT $4;`
+	return t.getManyTasksByQuery(ctx, query, status1.Int(), status2.Int(), offset, limit)
 }
 
 func (t *TaskRepository) PaginateWithoutStatus(ctx context.Context, offset int, limit int, ignored model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE status <> $1
+ORDER BY created_at DESC
+OFFSET $2 LIMIT $3;`
+	return t.getManyTasksByQuery(ctx, query, ignored.Int(), offset, limit)
 }
 
 func (t *TaskRepository) PaginateFilterWithStatus(ctx context.Context, offset int, limit int, task string, status model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE value LIKE $1 AND status = $2
+ORDER BY created_at DESC
+OFFSET $3 LIMIT $4;`
+	return t.getManyTasksByQuery(ctx, query, fmt.Sprintf("%%%s%", task), status.Int(), offset, limit)
 }
 
 func (t *TaskRepository) PaginateFilterWithStatuses(ctx context.Context, offset int, limit int, task string, status1, status2 model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE value LIKE $1 AND (status = $2 OR status = $3)
+ORDER BY created_at DESC
+OFFSET $4 LIMIT $5;`
+	return t.getManyTasksByQuery(ctx, query, fmt.Sprintf("%%%s%", task), status1.Int(), status2.Int(), offset, limit)
 }
 
 func (t *TaskRepository) PaginateFilterWithoutStatus(ctx context.Context, offset int, limit int, task string, ignored model.Status) ([]model.TaskDTO, error) {
-	//TODO implement me
-	panic("implement me")
+	const query = `SELECT id, value, created_at, status
+FROM tasks
+WHERE value LIKE $1 AND status <> $2
+ORDER BY created_at DESC
+OFFSET $3 LIMIT $4;`
+	return t.getManyTasksByQuery(ctx, query, fmt.Sprintf("%%%s%", task), ignored.Int(), offset, limit)
 }
