b108b3313aa9e5b24afb17b1351d38b00b7bd53e
Vlad Marlo <vlad.marlo@ya.ru>
2023-11-14 14:25:25 +0300

    added cors support

--- internal/controller/http/http.go
+++ internal/controller/http/http.go
@@ -49,6 +49,8 @@ func (ctrl *Controller) configure() {
 
 func (ctrl *Controller) configureMiddleWares() {
 	ctrl.router.Use(middleware.Logger())
+	ctrl.router.Use(middleware.CORS())
+	ctrl.router.Use(middleware.Recover())
 }
 
 //goland:noinspection ALL
@@ -69,7 +71,11 @@ func (ctrl *Controller) configureRoutes() {
 
 func (ctrl *Controller) Start(_ context.Context) error {
 	go func() {
-		ctrl.router.Start(fmt.Sprintf("%s:%d", ctrl.cfg.BindAddr, ctrl.cfg.BindPort))
+		ctrl.log.Error(
+			"got error while starting http server", zap.Error(
+				ctrl.router.Start(fmt.Sprintf("%s:%d", ctrl.cfg.BindAddr, ctrl.cfg.BindPort)),
+			),
+		)
 	}()
 	ctrl.log.Info("starting http server", zap.String("bind_addr", fmt.Sprintf("%s:%d", ctrl.cfg.BindAddr, ctrl.cfg.BindPort)))
 	return nil
